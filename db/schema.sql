72043d545b77ac48a0160b00b656c377f6ed5a1f
Carlos Jorge Gonzalez Perales <carlosgzzperales@gmail.com>
2024-06-17 08:10:30 -0600

    added values for department and roles tables

--- db/schema.sql
+++ db/schema.sql
@@ -5,10 +5,10 @@ CREATE DATABASE employee_db;
 
 CREATE TABLE department (
     id: SERIAL PRIMARY KEY,
-    name: VARCHAR(30) UNIQUE NOT NULL 
+    department_name: VARCHAR(30) UNIQUE NOT NULL 
 );
 
-CREATE TABLE role (
+CREATE TABLE roles (
     id: SERIAL PRIMARY KEY,
     title: VARCHAR(30) UNIQUE NOT NULL,
     salary: DECIMAL NOT NULL,
@@ -24,7 +24,7 @@ CREATE TABLE role (
         last_name: VARCHAR(30) NOT NULL,
         role_id: INTEGER NOT NULL,
         manager_id: INTEGER,
-        FOREIGN KEY (role_id) REFERENCES role (id)
+        FOREIGN KEY (role_id) REFERENCES roles (id)
         FOREIGN KEY (manager_id) REFERENCES employee (id)
         ON DELETE SET NULL
     );
---
+++ db/seeds.sql
@@ -0,0 +1,15 @@
+INSERT INTO department (id, department_name) VALUES
+(1, 'Sales'),
+(2, 'Legal'),
+(3, 'Engineering'),
+(4, 'Finance');
+
+INSERT INTO roles (id, title, salary) VALUES
+(1, 'Sales Lead', 100000),
+(2, 'Sales Person', 80000),
+(3, 'Legal Team Lead', 250000),
+(4, 'Lawyer', 190000),
+(5, 'Lead Engineer', 150000),
+(6, 'Software Engineer', 120000),
+(7, 'Account Manager', 160000),
+(8, 'Accountant', 125000);
